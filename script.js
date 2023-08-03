53d58a150e4e35880d063275588bc179116acddf
NaitikPatel <np03022005@gmail.com>
2023-08-03 18:43:44 +0530

    done the floating part of binary to base 9  10 to 16 is remaining

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function handleConversionType() {
 
     if (selectElement === "other") {
         b = prompt("Enter the custom base");
-        b = parseInt(b, 10); 
-    } 
+        b = parseInt(b, 10);
+    }
 }
 
 function conversion() {
@@ -14,33 +14,68 @@ function conversion() {
 
     if (selectElement === "binary") {
         b = 2;
-        b = parseInt(b, 10);
-    }
-    else if (selectElement === "octal") {
+    } else if (selectElement === "octal") {
         b = 8;
-        b = parseInt(b, 10);
-    }
-    else if (selectElement === "hexadecimal") {
+    } else if (selectElement === "hexadecimal") {
         b = 16;
-        b = parseInt(b, 10);
+    } else {
+        b = 10;
     }
 
-    var num = parseInt(document.getElementById('base-input').value);
+    var num = parseFloat(document.getElementById('base-input').value);
 
-    const a = [];
-    var i = 0;
-    while (num !== 0) {
-        var temp = num % b;
-        a[i] = temp;
-        num = Math.floor(num / b); 
-        i++;
-    }
-    
-    var ans = ""; 
-    while (i--) {
-        ans = ans + String(a[i]); 
-    }
+    var n1 = num - parseInt(num);
 
-    var p = document.getElementById('converted-number');
-    p.innerHTML ="The Number is converted into " + ans; 
+    var p = parseInt(num) //for fractional
+    if (n1 === 0) {
+        const a = [];
+        var i = 0;
+        while (num !== 0) {
+            var temp = num % b;
+            a[i] = temp;
+            num = Math.floor(num / b);
+            i++;
+        }
+
+        var ans = "";
+        while (i--) {
+            ans = ans + String(a[i]);
+        }
+
+        var p = document.getElementById('converted-number');
+        p.innerHTML = "The Number is converted into " + ans;
+    } else {
+
+
+        const a = [];
+        var i = 0;
+        while (p !== 0) {
+            var temp = p % b;
+            a[i] = temp;
+            p = Math.floor(p / b);
+            i++;
+        }
+
+        var ans = "";
+        while (i--) {
+            ans = ans + String(a[i]);
+        }
+
+        var f = num - Math.floor(num);
+        var ansf = "";
+
+
+        while (f !== 0 && ansf.length < 4) {
+            f = f * b;
+            ansf = ansf + Math.floor(f);
+            f = f - Math.floor(f);
+        }
+
+        // console.log(ans);
+        // console.log(ansf);
+
+        var p = document.getElementById('converted-number');
+        
+        p.innerHTML = "The Number is converted into " + ans + "." + ansf;
+    }
 }
