5104afc68732298e8a6711738c97e88e09fb77fa
NaitikPatel <np03022005@gmail.com>
2023-08-05 11:09:22 +0530

    dn css and radix system still there is sum bugs

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function conversion() {
     // console.log(k);
 
     if( document.getElementById('base-input').value == ''){
-        alert("Please enter the number")
+        alert("Please enter the number");
     }
     else{
         var num = parseFloat(document.getElementById('base-input').value);
@@ -59,138 +59,158 @@ function conversion() {
             alert("Invalid input. Please enter a valid number.");
             return;
         }
+        if(k!=10){
+            // console.log("hii0123");
+            var n3 = num - parseInt(num);
+            var p1 = parseInt(num);
+            var sum = 0;
+            const l=[];
+            while(p1 != 0){
+                var z = p1%10;
+                l.push(z);
+                p1 = Math.floor(p1 / 10);
+            }
+            for(let i=0;i<l.length;i++){
+                sum = sum + Math.pow(k,i) * l[i];
+            }
+            // console.log(sum);
+            
+            const l2=[];
+            var sumf = 0;
+            var j = 0;
+            // console.log(n3);
+            while(n3 != 0 && j<4){
+                n3 = n3*10
+                z = n3%10;
+                l2.push(z);
+                j++;
+            }
+        
+            for (let i = 0; i < l.length; i++) {
+                sumf = sumf + Math.pow(k, -(i + 1)) *parseInt(l2[i]);
+                console.log(sumf);
+            }
+            
+        }
 
         var n1 = num - parseInt(num);
 
-        var p = parseInt(num) 
-        if(k===10){
+        var p = sum; 
 
-            if (n1 === 0) {
-                const a = [];
-                var i = 0;
-                while (num !== 0) {
-                    var temp = num % b;
-                    if(temp<10){
-                        a[i]=temp;
-                    }
-                    else if(temp===10){
-                        a[i]='A';
-                    }
-                    else if(temp===11){
-                        a[i]='B';
-                    }
-                    else if(temp===12){
-                        a[i]='C';
-                    }
-                    else if(temp===13){
-                        a[i]='D';
-                    }
-                    else if(temp===14){
-                        a[i]='E';
-                    }
-                    else{
-                        a[i]='f';
-                    }
-                    num = Math.floor(num / b);
-                    i++;
+        if (n1 === 0) {
+            const a = [];
+            var i = 0;
+            num = p;
+            while (num !== 0) {
+                var temp = num % b;
+                if(temp<10){
+                    a[i]=temp;
                 }
-                let ans = "";
-                while(i--){
-                    ans = ans + String(a[i]);
+                else if(temp===10){
+                    a[i]='A';
                 }
-                var p = document.getElementById('converted-number');
-                p.innerHTML = `The Number is converted into ${ans}`;
-
-            }   
-            else 
-            {
-                const a = [];
-                var i = 0;
-                while (p !== 0) {
-                    var temp = p % b;
-
-                    // console.log(typeof temp);
-
-                    if(temp<10){
-                        a[i]=temp;
-                        console.log("hii",temp);
-                        }
-                    else if(temp===10){
-                        a[i]='A';
-                    }
-                    else if(temp===11){
-                        a[i]='B';
-                    }
-                    else if(temp===12){
-                        a[i]='C';
-                    }
-                    else if(temp===13){
-                        a[i]='D';
-                    }
-                    else if(temp===14){
-                        a[i]='E';
-                    }
-                    else{
-                        a[i]='f';
-                    }
-                    p = Math.floor(p/ b);
-                    i++;
-                
+                else if(temp===11){
+                    a[i]='B';
                 }
-
-                var ans = "";               
-                while (i--) {
-                    ans = ans + String(a[i]);
+                else if(temp===12){
+                    a[i]='C';
                 }
-
-                var f = num - Math.floor(num);
-                var ansf = "";
-            
-                while (f !== 0 && ansf.length < 4) {
-                    f = f * b;
-
-                    // console.log(typeof f);
-                    // console.log(typeof temp);
-
-                    f1 = parseInt(f);
-
-                    var n2;
-                    if(f1<10){
-                        n2 = f1;
-                        // console.log(n2);
-                    }
-                    else if(f1===10){
-                        n2='A';
-                    }
-                    else if(f1===11){
-                        n2='B';
-                    }
-                    else if(f1===12){
-                        n2='C';
-                    }
-                    else if(f1===13){
-                        n2='D';
-                    }
-                    else if(f1===14){
-                        n2='E';
-                    }
-                    else{
-                        n2='F';
-                    }
-                    ansf = ansf + n2 
-
-                    f = f - f1;
+                else if(temp===13){
+                    a[i]='D';
                 }
-            
-                // console.log(ans);
-                // console.log(ansf);
-
-                var p = document.getElementById('converted-number');
-
-                p.innerHTML = `The Number is converted into ${ans} . ${ansf}`;
+                else if(temp===14){
+                    a[i]='E';
+                }
+                else{
+                    a[i]='f';
+                }
+                num = Math.floor(num / b);
+                i++;
             }
+            let ans = "";
+            while(i--){
+                ans = ans + String(a[i]);
+            }
+            var p = document.getElementById('converted-number');
+            p.innerHTML = `The Number is converted into ${ans}`;
         }   
-
+        else 
+        {
+            const a = [];
+            var i = 0;
+            while (p !== 0) {
+                var temp = p % b;
+                // console.log(typeof temp);
+                if(temp<10){
+                    a[i]=temp;
+                    // console.log("hii",temp);
+                    }
+                else if(temp===10){
+                    a[i]='A';
+                }
+                else if(temp===11){
+                    a[i]='B';
+                }
+                else if(temp===12){
+                    a[i]='C';
+                }
+                else if(temp===13){
+                    a[i]='D';
+                }
+                else if(temp===14){
+                    a[i]='E';
+                }
+                else{
+                    a[i]='f';
+                }
+                p = Math.floor(p/ b);
+                i++;
+            
+            }
+            var ans = "";               
+            while (i--) {
+                ans = ans + String(a[i]);
+            }
+            var f = sumf;
+            var ansf = "";
+        
+            while (f !== 0 && ansf.length < 4) {
+                f = f * b;
+                // console.log(typeof f);
+                // console.log(typeof temp);
+                f1 = parseInt(f);
+                var n2;
+                if(f1<10){
+                    n2 = f1;
+                    // console.log(n2);
+                }
+                else if(f1===10){
+                    n2='A';
+                }
+                else if(f1===11){
+                    n2='B';
+                }
+                else if(f1===12){
+                    n2='C';
+                }
+                else if(f1===13){
+                    n2='D';
+                }
+                else if(f1===14){
+                    n2='E';
+                }
+                else{
+                    n2='F';
+                }
+                ansf = ansf + n2 
+                f = f - f1;
+            }
+        
+            // console.log(ans);
+            // console.log(ansf);
+            var p = document.getElementById('converted-number');
+            p.innerHTML = `The Number is converted into ${ans} . ${ansf}`;
+        }
 
     }
    
